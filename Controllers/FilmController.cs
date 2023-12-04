62bca8cf97fbe3c885e2ede23ec75f3690f40686
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2023-12-04 20:24:52 -0300

    Project initial

---
+++ Controllers/FilmController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace filmesApi;
+
+[ApiController]
+[Route("/films")]
+public class FilmController : ControllerBase
+{
+    private readonly ILogger<FilmController> _logger;
+    private readonly List<Film> films = new List<Film>();
+
+    public FilmController(ILogger<FilmController> logger) 
+    {
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public void addFilm([FromBody] Film film)
+    {
+        _logger.LogInformation("Add new film");
+        films.Add(film);
+        _logger.LogInformation("todos os filmes " + films);
+    }
+}
---
+++ Models/Film.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace filmesApi;
+
+public record Film([Required(ErrorMessage = "O campo Title é obrigatório")]
+                    string Title,
+                    
+                    [Required(ErrorMessage = "O campo Gender é obrigatório")]
+                    [MaxLength(50, ErrorMessage = "O campo Gender possuir no máximo 50 caracteres")]
+                    [MinLength(3, ErrorMessage = "O campo Gender deve possuir no mínimo 3 caracteres")]
+                    string Gender,
+                    
+                    [Required]
+                    [Range(60, 600, ErrorMessage = "O campo Duration deve possuir um valor entre 60 e 600")]
+                    int Duration);
